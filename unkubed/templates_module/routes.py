68287619796bc3fa50e692b7d3220573ab901b62
Ste Rose <git@sterose.io>
2026-04-05 21:44:59 +0100

    Inline template form into template routes

--- unkubed/templates_module/routes.py
+++ unkubed/templates_module/routes.py
@@ -4,15 +4,49 @@ import re
 
 from flask import Blueprint, abort, flash, render_template, request
 from flask_login import current_user, login_required
+from flask_wtf import FlaskForm
 from markupsafe import Markup, escape
+from wtforms import BooleanField, IntegerField, SelectField, StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length, NumberRange, Optional
 import yaml
 
 from ..extensions import db
 from ..models import SavedTemplate
 from ..services.kube import KubectlService, get_active_cluster
-from .forms import TemplateForm
 
 templates_bp = Blueprint("templates", __name__, template_folder="../templates/templates")
+
+
+class TemplateForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired(), Length(max=150)])
+    namespace = StringField(
+        "Namespace",
+        validators=[DataRequired(), Length(max=120)],
+        default="default",
+    )
+    image = StringField("Container Image", validators=[Optional(), Length(max=200)])
+    replicas = IntegerField("Replicas", validators=[Optional(), NumberRange(min=1)], default=1)
+    container_port = IntegerField(
+        "Container Port", validators=[Optional(), NumberRange(min=1, max=65535)], default=80
+    )
+    service_type = SelectField(
+        "Service Type",
+        choices=[("ClusterIP", "ClusterIP"), ("NodePort", "NodePort"), ("LoadBalancer", "LoadBalancer")],
+        validators=[Optional()],
+        validate_choice=False,
+    )
+    service_port = IntegerField(
+        "Service Port", validators=[Optional(), NumberRange(min=1, max=65535)], default=80
+    )
+    target_port = IntegerField(
+        "Target Port", validators=[Optional(), NumberRange(min=1, max=65535)], default=80
+    )
+    config_data = TextAreaField(
+        "ConfigMap data (key=value per line)",
+        validators=[Optional()],
+    )
+    save_template = BooleanField("Save this template")
+    submit = SubmitField("Generate YAML")
 
 
 @templates_bp.route("/")
