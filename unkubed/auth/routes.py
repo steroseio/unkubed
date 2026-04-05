37d1b8ec225c63afd710f6041930834e484c1258
Ste Rose <git@sterose.io>
2026-04-05 21:46:59 +0100

    Inline auth forms into auth routes

--- unkubed/auth/routes.py
+++ unkubed/auth/routes.py
@@ -1,11 +1,30 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 from ..extensions import db
 from ..models import User
-from .forms import LoginForm, RegisterForm
 
 auth_bp = Blueprint("auth", __name__, template_folder="../templates/auth")
+
+
+class RegisterForm(FlaskForm):
+    full_name = StringField("Full Name", validators=[Length(max=120)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=8)])
+    confirm_password = PasswordField(
+        "Confirm Password",
+        validators=[DataRequired(), EqualTo("password", "Passwords must match.")],
+    )
+    submit = SubmitField("Create account")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Sign in")
 
 
 @auth_bp.route("/register", methods=["GET", "POST"])
